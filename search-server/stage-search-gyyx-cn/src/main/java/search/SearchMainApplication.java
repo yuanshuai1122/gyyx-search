007ab068475e60572b1227f5ee40569c4be16705
yuanshuai <yuanshuai@gyyx.cn>
2023-08-07 09:48:34 +0800

    重启后端项目

---
+++ search-server/stage-search-gyyx-cn/src/main/java/search/SearchMainApplication.java
@@ -0,0 +1,16 @@
+package search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yuanshuai
+ */
+@SpringBootApplication
+public class SearchMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchMainApplication.class, args);
+    }
+
+}
---
+++ search-server/stage-search-gyyx-cn/src/test/java/search/SearchMainApplicationTests.java
@@ -0,0 +1,13 @@
+package search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
